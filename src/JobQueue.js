0f0ce4bf33ff6c4533bccd58c502b03415ceb398
ENvironmentSet <sbshw1@gmail.com>
2019-01-28 12:53:23 +0900

    refactor JobQueue

--- src/JobQueue.js
+++ src/JobQueue.js
@@ -1,36 +1,73 @@
-export default class JobQueue {
-  constructor(dependencies = {}) {
-    this._queue = []; // @TODO: do optimization via numeral system
+class PackedPendingJobs {
+  constructor(reservedTime, ...pendingJobs) {
+    this.reservedTime = reservedTime;
+    this.pendingJobs = pendingJobs;
   }
 
-  enqueueJob(pendingJob) {
-    const { priority, reservedTime } = pendingJob;
-    const jobQueue = this._queue;
-    let currentIndex = jobQueue.length - 1;
+  insertPendingJob(pendingJob) {
+    const pendingJobs = this.pendingJobs;
+    let currentIndex = pendingJobs.length - 1;
     let inserted = false;
 
     while(!inserted) {
-      const currentPendingJob = jobQueue[currentIndex];
+      const currentPendingJob = pendingJob[currentIndex];
 
-      if (currentPendingJob.reservedTime === reservedTime && currentPendingJob.priority > priority) {
+      if (currentPendingJob.priority >= pendingJob) {
         const insertPosition = currentIndex + 1;
 
-        jobQueue.splice(insertPosition, 0, pendingJob);
+        pendingJobs.splice(insertPosition, 0, pendingJob);
         inserted = true;
       }
-      currentIndex -= 1;
+    }
+  }
+
+  get unpack() {
+    return this.pendingJobs;
+  }
+}
+
+export default class JobQueue {
+  constructor(dependencies = {}) {
+    this._packedPendingJobsQueue = [];
+  }
+
+  enqueueJob(pendingJob) {
+    const { reservedTime } = pendingJob;
+    const packedPendingJobsQueue = this._packedPendingJobsQueue;
+
+    if (packedPendingJobsQueue.length === 0) {
+      const packedPendingJobs = new PackedPendingJobs(reservedTime, pendingJob);
+
+      packedPendingJobsQueue.push(packedPendingJobs);
+    } else {
+      let currentIndex = packedPendingJobsQueue.length - 1;
+      let inserted = false;
+
+      while (!inserted) {
+        const currentPackedPendingJob = packedPendingJobsQueue[currentIndex];
+
+        if (currentPackedPendingJob.reservedTime === reservedTime) {
+          currentPackedPendingJob.insertPendingJob(pendingJob);
+          inserted = true;
+        } else if (currentPackedPendingJob.reservedTime < reservedTime) {
+          const packedPendingJobs = new PackedPendingJobs(reservedTime, pendingJob);
+          const insertPosition = currentIndex + 1;
+
+          packedPendingJobsQueue.splice(insertPosition, 0 , packedPendingJobs);
+          inserted = true;
+        }
+        currentIndex -= 1;
+      }
     }
   }
 
   dequeueJob() {
-    const jobQueue = this._queue;
+    const packedPendingJobsQueue = this._packedPendingJobsQueue;
+    const packedPendingJobs = packedPendingJobsQueue[0];
+    const pendingJobs = packedPendingJobs.unpack;
+    const pendingJob = pendingJobs.shift() || null;
 
-    return jobQueue.shift();
-  }
-
-  get oldestPendingJob() {
-    const jobQueue = this._queue;
-
-    return jobQueue[0] || null;
+    if (pendingJobs.length === 0) packedPendingJobsQueue.shift();
+    return pendingJob;
   }
 };
