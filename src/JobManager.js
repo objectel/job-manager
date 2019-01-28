0635e5469528edbe922a894d1f8c62b77136bbc4
ENvironmentSet <sbshw1@gmail.com>
2019-01-29 08:37:30 +0900

    Add JobManager

---
+++ src/JobManager.js
@@ -0,0 +1,48 @@
+function defaultJobResolver(jobManager) {
+  const time = jobManager.time;
+
+  for (const jobQueue of jobManager) {
+    const nextReservedTime = jobQueue.nextReservedTime;
+
+    if (time === nextReservedTime) {
+      while(time === jobQueue.nextReservedTime) {
+        const pendingJob = jobQueue.dequeueJob();
+        const job = pendingJob.job;
+
+        job();
+      }
+    }
+  }
+}
+
+export default class JobManager {
+  constructor(jobResolver = defaultJobResolver) {
+    this._jobQueueMap = new Map();
+    this.time = 0;
+    setInterval(() => {
+      this.time += 10;
+      jobResolver(this);
+    }, 10);
+  }
+
+  addJobQueue(identifier, jobQueue) {
+    const jobQueueMap = this._jobQueueMap;
+
+    jobQueueMap.set(identifier, jobQueue);
+    return this;
+  }
+
+  enqueueJob(jobQueueIdentifier, pendingJob) {
+    const jobQueueMap = this._jobQueueMap;
+    const jobQueue = jobQueueMap.get(jobQueueIdentifier);
+
+    jobQueue.enqueueJob(pendingJob);
+    return this;
+  }
+
+  get [Symbol.iterator]() {
+    const jobQueueMap = this._jobQueueMap;
+
+    return jobQueueMap[Symbol.iterator];
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as priorities } from './priorities';
 export { default as PendingJob } from './PendingJob';
 export { default as JobQueue } from './JobQueue';
+export { default as JobManager } from './JobManager';
